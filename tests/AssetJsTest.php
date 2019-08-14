e4c3553a2f23bc65118a4574dec1f4f4b375c9bc
Oleg <boxmilo@gmail.com>
2019-08-14 08:11:15 +0600

    asset

---
+++ tests/AssetJsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Description of AssetJsTest
+ *
+ * @author oleg
+ */
+class AssetJsTest extends TestCase{
+    
+    public function testLoad() {
+        $this->assertTrue(false);
+    }
+}
